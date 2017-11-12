5571c7811fca5131f9a91ce774dc7968bb783aef
smhdale <smhdale@live.com.au>
2017-11-12 14:36:30 +1000

    Fixed distBetween function

--- src/trig.js
+++ src/trig.js
@@ -1,7 +1,4 @@
-function distBetween (x1, y1, x2, y2) {
-  return Math.sqrt((x2 - x1) ** 2, (y2 - y1) ** 2)
-}
-
+const distBetween = (x1, y1, x2, y2) => ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 const lengthdirX = (len, dir) => len * Math.cos(dir)
 const lengthdirY = (len, dir) => len * Math.sin(dir)
 
